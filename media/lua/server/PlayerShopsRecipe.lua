5fcfb07542173a2eaa34d508362ac51b4fd69c47
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2024-02-20 22:52:42 +0000

    fix errors if recipe's original ontest does not exist

--- media/lua/server/PlayerShopsRecipe.lua
+++ media/lua/server/PlayerShopsRecipe.lua
@@ -1,3 +1,4 @@
+---@param func function
 local generateOnTest = function(func)
     ---@param item InventoryItem
     return function(item, ...)
@@ -6,20 +7,33 @@ local generateOnTest = function(func)
     end
 end
 
+---@param funcName string
+---@return function?
+local function getFunctionByName(funcName)
+    local funcLocation = luautils.split(funcName, "%.")
+    local func = _G
+    for j = 1, #funcLocation do
+        func = func[funcLocation[j]]
+        if not func then return end
+    end
+    if not type(func) == "function" then return end
+    return func
+end
+
 local recipes = ScriptManager.instance:getAllRecipes()
 for i = 0, recipes:size()-1 do
     ---@type Recipe
     local recipe = recipes:get(i)
     local onTest = recipe:getLuaTest()
     if onTest and onTest ~= "" then
-        local funcLocation = luautils.split(onTest, "%.")
-        local func = _G
-        for j = 1, #funcLocation do
-            func = func[funcLocation[j]]
+        local func = getFunctionByName(onTest)
+        if func then
+            local newName = string.gsub(onTest, "%.", "%$")
+            __PlayerShopsOnTest[newName] = generateOnTest(func)
+            recipe:setLuaTest("__PlayerShopsOnTest." .. newName)
+        else
+            recipe:setLuaTest("__PlayerShopsOnTest.HasAccessToShop")
         end
-        local newName = string.gsub(onTest, "%.", "%$")
-        __PlayerShopsOnTest[newName] = generateOnTest(func)
-        recipe:setLuaTest("__PlayerShopsOnTest." .. newName)
     else
         recipe:setLuaTest("__PlayerShopsOnTest.HasAccessToShop")
     end
